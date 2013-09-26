13cf1a2389c1c4c3f6c81fc3686f013569c1b18f
Juan Girini <juan@barracuda-digital.co.uk>
2013-09-26 12:40:28 +0100

    License modification

--- lib/Raven.php
+++ lib/Raven.php
@@ -5,6 +5,26 @@
  * API documentation:
  * https://api.raventools.com/docs/
  *
+ *
+ * @copyright Juan Girini 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * LIMITATIONS:
+ * Rate Limiting: Each API key/IP address is allowed 100 requests per minute. 
+ * If you exceed the limit, an HTTP 503 error response will be returned.
+ *
  * @author  Juan Girini <juan.girini@gmail.com>
  * @link    https://github.com/juangirini/raven-php-lib
  * @version 1.0
